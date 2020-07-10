3873e051504b22784a906e82e13cbd4ac9bf9810
Milan Popadic <mlnpopadic@gmail.com>
2020-07-10 17:51:02 +0200

    drawer, stack navigation

--- screens/drugaGodina.js
+++ screens/drugaGodina.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function DrugaGodina() {
+export default function DrugaGodina({navigation}) {
+    
+    const pressHandler = () => {
+        navigation.navigate('OglasDetail');
+    }
+
     return (
         <View>
             <Text>DrugaGodina screen</Text>
+            <Button title='vidi oglas detaljno' onPress={pressHandler}/>
         </View>
     )
 }
---
+++ screens/oglasDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+export default function OglasDetail({ navigation }) {
+  //const rating = navigation.getParam('rating'); get param snippet
+  
+  return (
+    <View>
+      <Text>Oglas detaljno</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
